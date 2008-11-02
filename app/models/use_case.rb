dda296ea3f9cdd75125048531bdd407919235b9f
Darren Hinderer <darren.hinderer@gmail.com>
2008-11-02 00:29:13 -0600

    support delete step

--- app/models/use_case.rb
+++ app/models/use_case.rb
@@ -3,4 +3,17 @@ class UseCase < ActiveRecord::Base
   has_many :steps, :order => "position"
 
   validates_presence_of :name, :plan_id
+
+  def update_positions_for_delete(delete_position)
+  begin
+    for step in steps
+      if step.position > delete_position
+        step.position = step.position - 1
+        step.save
+      end
+    end
+  rescue => e
+    puts e
+  end
+  end
 end
