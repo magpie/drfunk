fe5be2f46f1c0c920e9b7f137b1c6b93620cfa10
Darren Hinderer <darren@pigeon.(none)>
2008-11-02 14:48:58 -0700

    added textilize stuff for in place edit.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,3 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-
- # taken from:
- # http://blog.codahale.com/2006/01/14/a-rails-howto-simplify-in-place-editing-with-scriptaculous/
- def editable_content(options)
-   options[:content] = { :element => 'span' }.merge(options[:content])
-   options[:url] = {}.merge(options[:url])
-   options[:ajax] = { :okText => "'Save'", :cancelText => "'Cancel'"}.merge(options[:ajax] || {})
-   script = Array.new
-   script << "new Ajax.InPlaceEditor("
-   script << "  '#{options[:content][:options][:id]}',"
-   script << "  '#{url_for(options[:url])}',"
-   script << "  {"
-   script << options[:ajax].map{ |key, value| "#{key.to_s}: #{value}" }.join(", ")
-   script << "  }"
-   script << ")"
-
-   content_tag(
-     options[:content][:element],
-     options[:content][:text],
-     options[:content][:options]
-   ) + javascript_tag( script.join("\\n") )
- end
-
 end
