5b9b2db9ffdcddd0d5c3cfdae8811fa8d8fc78c3
Darren Hinderer <darren.hinderer@gmail.com>
2008-11-23 21:29:06 -0700

    features can be deleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :features
-  map.resources :steps, 
-    :member => {:unformatted_description => :put, :unformatted_expected => :put}
+  map.resources :steps
   map.resources :scenarios, 
     :member => {:update_step_order => :put, :update_feature => :put}
   map.resources :plans
