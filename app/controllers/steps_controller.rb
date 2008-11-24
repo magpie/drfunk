a1018dcb852a991cd8b1c4a6e2432e7daffb1660
Darren Hinderer <darren@pigeon.(none)>
2008-11-23 20:45:47 -0700

    in place editing done right for steps

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,29 +1,30 @@
 class StepsController < ApplicationController
 
-  def edit
-    @step = Step.find(params[:id])
-  end
-
   def create
     @step = Step.new(params[:step])
     @scenario  = Scenario.find(@step.scenario_id)
     @step.position = @scenario.steps.length + 1
     @step.save
 
-    #refresh
-    @scenario  = Scenario.find(@step.scenario_id)
+    @scenario.reload
+  end
+ 
+  def show
+    @step = Step.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def edit
+    @step = Step.find(params[:id])
   end
 
   def update
-    step = Step.find(params[:id])
-     
-    step.update_attributes(params[:step])
-    step.reload
-    unless params[:step][:description].nil?
-      render :text => textilize_without_paragraph(step.description)
-    else
-      render :text => textilize_without_paragraph(step.expected)
-    end
+    @step = Step.find(params[:id])
+    @step.update_attributes(params[:step])
   end
 
   def destroy
@@ -32,27 +33,7 @@ class StepsController < ApplicationController
     @scenario.update_positions_for_delete(@step.position)
     @step.destroy
 
-    #refresh
-    @scenario  = Scenario.find(@step.scenario_id)
+    @scenario.reload
   end
 
-  def unformatted_description
-    step = Step.find(params[:id])
-    render :text => step.description
-  end
-
-  def unformatted_expected
-    step = Step.find(params[:id])
-    render :text => step.expected
-  end
-
-  private
-
-  # rails helper method was not accessible in controller
-  def textilize_without_paragraph(text)
-    textiled = textilize(text)
-    if textiled[0..2] == "<p>" then textiled = textiled[3..-1] end
-    if textiled[-4..-1] == "</p>" then textiled = textiled[0..-5] end
-    return textiled
-  end
 end
