5f989e3c7041e526371221912db861c11a0d7220
Darren Hinderer <darren@pigeon.(none)>
2008-11-30 12:28:48 -0700

    use nested routes where it makes sense.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
+
   map.resources :features
-  map.resources :steps
-  map.resources :scenarios, 
-    :member => {:update_step_order => :put, :update_feature => :put}
-  map.resources :plans
+  map.resources :plans, :shallow => true do |plan|
+    plan.resources :scenarios, :member => {:update_step_order => :put, :update_feature => :put} do |scenario|
+      scenario.resources :steps
+    end
+  end
 
   map.root :controller => "plans"
   map.connect ':controller/:action/:id'
