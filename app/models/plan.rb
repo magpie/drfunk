03b74e92944f742b69a69974698e3a7e261fdd40
Darren Hinderer <darren.hinderer@gmail.com>
2008-12-07 11:29:39 -0700

    plans can be deleted

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,6 @@
 class Plan < ActiveRecord::Base
-  has_many :scenarios
-  has_many :features
+  has_many :scenarios, :dependent => :destroy
+  has_many :features, :dependent => :destroy
   
   validates_presence_of :name
   named_scope :name_sorted, :order => "name"
