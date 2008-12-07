fbc9b8206b4ee60b5821f5101a573f2ea4338c6e
Darren Hinderer <darren.hinderer@gmail.com>
2008-12-07 13:52:37 -0700

    page titles

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,4 +4,12 @@ class Plan < ActiveRecord::Base
   
   validates_presence_of :name
   named_scope :name_sorted, :order => "name"
+
+  def step_count
+    step_counter = 0
+    scenarios.each do |scenario|
+      step_counter += scenario.steps.count  
+    end
+    step_counter
+  end
 end
