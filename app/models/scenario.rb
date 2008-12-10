7b892c8832cda10f6910adeadefd8dee57bbd6dd
Darren Hinderer <darren@pigeon.(none)>
2008-12-09 19:06:20 -0700

    scenarios are added underneath features only

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -2,19 +2,13 @@ class Scenario < ActiveRecord::Base
   belongs_to :plan
   belongs_to :feature
   has_many :steps, :dependent => :destroy, :order => "position"
+  before_create :set_position
 
-  before_validation_on_create :set_feature
   validates_presence_of :name, :plan_id, :feature_id
-  
+
   private
 
-  def set_feature 
-    if self.plan.features.size == 0
-      @f = Feature.create( {:name => "New Feature", :plan_id => self.plan_id})
-    else
-      @f = self.plan.features.last_updated.first
-    end
-
-    self.feature_id = @f.id
+  def set_position
+    self.position = feature.scenarios.length + 1
   end
 end
