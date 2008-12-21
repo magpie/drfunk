eb04a054572be50940c4eb7cfcf60baca1a5ae59
Darren Hinderer <darren@pigeon.(none)>
2008-12-21 11:13:58 -0700

    rework setup ui to make it less confusing.

--- app/controllers/setups_controller.rb
+++ app/controllers/setups_controller.rb
@@ -1,36 +1,21 @@
 class SetupsController < ApplicationController
   
-  def create
-    @scenario = Scenario.find(params[:scenario_id])
-    @scenario.update_attribute(:setup, params[:scenario][:setup])
-  end
-
-  def edit_copy
-    @scenario = Scenario.find(params[:scenario_id])
-  end
-
-  def copy
-    @scenario = Scenario.find(params[:scenario_id])
-    other_setup = Scenario.find(params[:other_scenario_id]).setup
-    @scenario.update_attribute(:setup, other_setup)
-  end
-
   def edit
     @scenario = Scenario.find(params[:scenario_id])
   end
   
   def update
     @scenario = Scenario.find(params[:scenario_id])
-    @scenario.update_attribute(:setup, params[:scenario][:setup])
+    if params[:other_scenario_id] == "place_holder"
+      @scenario.update_attribute(:setup, params[:scenario][:setup])
+    else
+      other_scenario = Scenario.find(params[:other_scenario_id])
+      @scenario.update_attribute(:setup, other_scenario.setup)
+    end
   end
 
   def show
     @scenario = Scenario.find(params[:scenario_id])
-  end
-
-  def destroy
-    @scenario = Scenario.find(params[:scenario_id])
-    @scenario.update_attribute(:setup, nil)
   end
 
 end
