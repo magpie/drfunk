4ebcb61792ee5c32a128739a203efa1e1ff76c8b
Darren Hinderer <darren@pigeon.(none)>
2008-12-26 17:33:41 -0700

    continuation of bug fix

--- app/controllers/setups_controller.rb
+++ app/controllers/setups_controller.rb
@@ -7,7 +7,7 @@ class SetupsController < ApplicationController
   def update
     @scenario = Scenario.find(params[:scenario_id])
 
-    if params[:other_scenario_id] == "none_selected"
+    if params[:other_scenario_id].nil? || params[:other_scenario_id] == "none_selected"
       @scenario.update_attribute(:setup, params[:scenario][:setup])
     else
       @scenario.copy_setup(params[:other_scenario_id])
