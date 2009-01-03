e5db03c3385afbaa90035e6d1f170c0b6f4a1520
Darren Hinderer <darren@pigeon.(none)>
2009-01-02 23:11:04 -0700

    add and remove was jacking up the hover

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,42 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+var ho;
+
+Event.observe(window, 'load', onWindowLoad);
+Event.observe(window, 'click', function(){ho.clear()});
+
+
+function onWindowLoad(event) {
+  ho = new HoverObserver("scenario");
+}
+
+var HoverObserver = Class.create({
+  initialize: function(css_class){
+    this.css_selector = "." + css_class;
+    this.start();
+  },
+  
+  start: function() {
+    $$(this.css_selector).each(function(scenario) {
+      scenario.observe('mouseover', function(){this.addClassName('active');});
+      scenario.observe('mouseout', function(){this.removeClassName('active');});
+    }); 
+    
+  },
+
+  stop: function() {
+    $$(this.css_selector).each(function(scenario) {
+      scenario.stopObserving('mouseover');
+      scenario.stopObserving('mouseout');
+    }); 
+
+  },
+
+  clear: function(scenario_name) {
+    $$(this.css_selector).each(function(scenario) {
+      scenario.removeClassName('active');
+    }); 
+  }
+
+});
