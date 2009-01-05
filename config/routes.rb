d8c88d2c5c6ed3b37edf7cd2b2f91d9cd87dbb61
Darren Hinderer <darren@pigeon.(none)>
2009-01-04 23:37:43 -0700

    add search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.resources :features, :member => {:update_scenario_order => :put}
-  map.resources :plans, :shallow => true do |plan|
+  map.resources :plans, :member => {:search => :get }, :shallow => true do |plan|
     plan.resources :scenarios, :member => {:update_step_order => :put, :duplicate => :post} do |scenario|
       scenario.resources :steps
       scenario.resource :setup
