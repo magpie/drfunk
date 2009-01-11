eb156f08e5ea01066596fe27ffe246adb4d6554a
Darren Hinderer <darren@pigeon.(none)>
2009-01-11 15:29:29 -0700

    test requirement controller

--- app/controllers/requirements_controller.rb
+++ app/controllers/requirements_controller.rb
@@ -1,14 +1,26 @@
 class RequirementsController < ApplicationController
   def edit
     @scenario = Scenario.find(params[:scenario_id])
+
+    respond_to do |format|
+      format.js
+    end
   end
   
   def update
     @scenario = Scenario.find(params[:scenario_id])
     @scenario.update_attribute(:requirement, params[:scenario][:requirement])
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def show
     @scenario = Scenario.find(params[:scenario_id])
+
+    respond_to do |format|
+      format.js
+    end
   end
 end
--- test/functional/requirements_controller_test.rb
+++ test/functional/requirements_controller_test.rb
@@ -4,4 +4,26 @@ class RequirementsControllerTest < ActionController::TestCase
   should_route :get, '/scenarios/1/requirement/edit', :controller => :requirements, :action => :edit, :scenario_id => 1
   should_route :get, '/scenarios/1/requirement', :controller => :requirements, :action => :show, :scenario_id => 1
   should_route :put, '/scenarios/1/requirement', :controller => :requirements, :action => :update, :scenario_id => 1
+
+  def test_edit_ajax
+    scenario = Factory(:scenario)
+    xhr :get, :edit, :scenario_id => scenario.id
+    assert_response :success
+    assert_select_rjs :chained_replace_html, "requirement"
+  end
+
+  def test_update_ajax
+    scenario = Factory(:scenario)
+    xhr :put, :update, :scenario_id => scenario.id, :scenario => {:requirement => "#ABC 123.4"}
+    assert_response :success
+    assert_select_rjs :chained_replace_html, "requirement"
+  end
+
+  def test_show_ajax
+    scenario = Factory(:scenario)
+    xhr :get, :show, :scenario_id => scenario.id
+    assert_response :success
+    assert_select_rjs :chained_replace_html, "requirement"
+  end
+
 end
