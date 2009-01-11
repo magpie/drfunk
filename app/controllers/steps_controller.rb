9fbbfaf2481c2c4c51dc0fa9ff64374f0e93b7e4
Darren Hinderer <darren.hinderer@gmail.com>
2009-01-11 14:54:29 -0700

    test scenarios controller

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -2,30 +2,54 @@ class StepsController < ApplicationController
 
   def index
     @scenario = Scenario.find(params[:scenario_id])
+
+    respond_to do |format|
+      format.html
+    end
   end
 
   def create
     @scenario = Scenario.find(params[:scenario_id])
     @step = @scenario.steps.create(params[:step])
     @scenario.reload
+
+    respond_to do |format|
+      format.js
+    end
   end
  
   def show
     @step = Step.find(params[:id])
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def edit
     @step = Step.find(params[:id])
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def update
     @step = Step.find(params[:id])
     @step.update_attributes(params[:step])
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
     @step = Step.destroy(params[:id])
     @scenario = Scenario.find(@step.scenario.id)
+
+    respond_to do |format|
+      format.js
+    end
   end
 
 end
