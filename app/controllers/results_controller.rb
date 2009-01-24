2bc7f7cdc8062cb8bc62e9d140c0a6cfe13cba0b
Darren Hinderer <darren@pigeon.(none)>
2009-01-24 16:25:50 -0700

    it seems like the update is not detected on the scenario

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,6 +3,7 @@ class ResultsController < ApplicationController
   def update
     @scenario = Scenario.find(params[:scenario_id])
     @scenario.update_attribute(:result, params[:result])
+    @scenario = @scenario.reload
 
     respond_to do |format|
       format.js
