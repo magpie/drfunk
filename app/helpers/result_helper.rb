ac3308d7b385923cbea86dcc6ad2f30a042e7c2a
Darren Hinderer <darren.hinderer@gmail.com>
2009-01-25 16:53:19 -0700

    fix null result status icon bug

--- app/helpers/result_helper.rb
+++ app/helpers/result_helper.rb
@@ -2,12 +2,12 @@ module ResultHelper
 
   def result_class(result)
     case result
-    when Scenario::RESULT_UNTESTED
-      "clear-link"
     when Scenario::RESULT_PASSED
       "pass-link"
     when Scenario::RESULT_FAILED
       "fail-link"
+    else
+      "clear-link"
     end
   end
 
