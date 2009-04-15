775518907447d3241cef7cd9508916f6cfb4f8a1
Darren Hinderer <darren.hinderer@gmail.com>
2009-04-14 23:09:20 -0600

    add step insert

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,7 +2,7 @@ class Step < ActiveRecord::Base
   belongs_to :scenario
   before_create :set_position
   before_save :timestamp_scenario
-  before_destroy :update_step_positions, :timestamp_scenario
+  before_destroy :update_step_positions_destroy, :timestamp_scenario
   validates_presence_of :scenario_id
 
   def search query
@@ -22,10 +22,23 @@ class Step < ActiveRecord::Base
   end
 
   def set_position
-    self.position = scenario.steps.length + 1
+    if self.position
+      update_step_positions_create
+    else
+      self.position = scenario.steps.length + 1
+    end
   end
 
-  def update_step_positions
+  def update_step_positions_create
+    scenario.steps.each do |step|
+      if step.position >= position
+        step.position = step.position + 1
+        step.save
+      end
+    end
+  end
+
+  def update_step_positions_destroy
     scenario.steps.each do |step|
       if step.position > position
         step.position = step.position - 1
