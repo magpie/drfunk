e05d57a158d1df31c8659784e710f7a0e387f629
Darren Hinderer <darren.hinderer@gmail.com>
2009-05-10 00:06:58 -0600

    switch to size, as empty doesn't seem to work in all cases

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,17 +1,18 @@
 class Feature < ActiveRecord::Base
   belongs_to :plan
   has_many :scenarios, :order => "position"
-  
+
   validates_presence_of :plan_id, :name
 
   named_scope :name_sorted, :order => "name"
   before_destroy :check_for_scenarios
 
   def check_for_scenarios
-    self.scenarios.empty?
+    self.scenarios.size == 0
   end
 
   def scenarios_tested
     scenarios.size > 0 && scenarios.size == scenarios.tested.size
   end
 end
+
