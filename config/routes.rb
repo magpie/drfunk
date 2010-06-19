fffd053214a0bd058051a83069125f26244165f4
Darren Hinderer <darren.hinderer@gmail.com>
2010-06-18 19:44:41 -0700

    make use of new shallow routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,24 +17,21 @@ Drfunk::Application.routes.draw do |map|
       post :create_from_xml
     end
 
-    resources :scenarios, :only => [:index, :new, :create]
-  end
+    shallow do
+      resources :scenarios do
+        member do
+          post :duplicate
+          put :update_step_order
+        end
 
-  resources :scenarios, :only => [:show, :edit, :update, :destroy] do
-    member do
-      post :duplicate
-      put :update_step_order
+        resources :steps
+        resource :setup
+        resource :requirement
+        resource :result
+      end
     end
-
-    resources :steps, :only => [:index, :new, :create]
-    resource :setup
-    resource :requirement
-    resource :result
   end
-
-  resources :steps, :only => [:show, :edit, :update, :destroy]
 
   match '/credits' => 'plans#credits', :as => :credits
   match '/' => 'plans#index'
-#  match '/:controller(/:action(/:id))'
 end
