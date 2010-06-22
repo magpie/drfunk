b73b669733494e5044450008a475fbf0bd004049
Darren Hinderer <darren.hinderer@gmail.com>
2010-06-21 20:13:23 -0700

    convert reqults to jquery

--- test/functional/results_controller_test.rb
+++ test/functional/results_controller_test.rb
@@ -10,6 +10,6 @@ class ResultsControllerTest < ActionController::TestCase
     step = Factory(:step)
     xhr :put, :update, :scenario_id => step.scenario.id, :result => "pass"
     assert_response :success
-    assert_select_rjs :chained_replace_html, "sidebar"
+    assigns(:scenario)
   end
 end
